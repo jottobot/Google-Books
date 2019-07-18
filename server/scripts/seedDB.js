94544f4aa73ff1f215027d670ce1d038ebf40c7f
Jordynn Otto <j_taylor92@yahoo.com>
2019-07-18 13:09:20 -0700

    db

--- server/scripts/seedDB.js
+++ server/scripts/seedDB.js
@@ -123,14 +123,27 @@ const bookSeed = [
   }
 ];
 
+
 db.Book
-  .remove({})
-  .then(() => db.Book.collection.insertMany(bookSeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+.create({})
+.then(() => db.Book.collection.insertMany(bookSeed))
+.then(data => {
+  console.log(data.result.n + " records inserted!");
+  process.exit(0);
+})
+.catch(err => {
+  console.error(err);
+  process.exit(1);
+});
+
+// db.Book
+//   .remove({})
+//   .then(() => db.Book.collection.insertMany(bookSeed))
+//   .then(data => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//     process.exit(1);
+//   });
